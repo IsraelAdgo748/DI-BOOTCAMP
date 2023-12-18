69634c4d345a95e4bf21315027b486710c4c05b8
israel <israel748@gmail.com>
2023-12-18 20:11:28 +0200

    week3 day2

---
+++ week3/day 1/script.js
@@ -0,0 +1,40 @@
+let thing = 'hello world'
+
+console.log(thing);
+
+let a = 5
+let b = 5
+let c = 5+10
+let d = null;
+let e = true;
+
+// console.log(a+b);
+// console.log(a+thing);
+// console.log(d);
+// console.log(e);
+
+
+// let longString = "This is a very long string which needs " +
+//                  "to wrap across multiple lines because " +
+//                  "otherwise, my code is unreadable.";
+
+// console.log(longString.length);
+
+// let string = `look it's number ${a}`;
+
+// console.log(string.indexOf('o'));
+
+let address = 'addressNumber';
+
+console.log(address);
+
+let street = 'addressStreet';
+
+console.log(street);
+
+let number = 'countryumber';
+
+console.log(number);
+
+
+let globalAddress = 'address';
---
+++ week3/day 2/script.js
@@ -0,0 +1,20 @@
+
+
+
+let food = 'pizza'
+
+//console.log(food);
+
+let meal = 'dinner'
+
+//console.log(meal);
+
+console.log("i ate " + food + "at every " + meal);
+
+
+
+let c = 18
+
+let f = c/5*9+32
+
+console.log(`${c}°C is ${f}°F`);
