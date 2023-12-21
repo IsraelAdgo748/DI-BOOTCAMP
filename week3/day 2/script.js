9ff82f2fb0808948161fe6d149883c9c87e16a67
israel <israel748@gmail.com>
2023-12-21 21:23:42 +0200

    week3 day3

--- week3/day 2/script.js
+++ week3/day 2/script.js
@@ -12,9 +12,33 @@ let meal = 'dinner'
 console.log("i ate " + food + "at every " + meal);
 
 
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"]
+
+console.log(myWatchedSeries.length);
+
+let bm  = "black mirror"
+let mh = "money heist"
+let bb = "the big bang theory" 
+let fr = "friends"
+
+console.log(bm , mh , "and " + bb);
+
+console.log("I watched 3 series " + bm , mh , "and " + bb );
+
+myWatchedSeries.splice(2, 1, fr)
+
+myWatchedSeries.push("dark")
+
+console.log(myWatchedSeries);
+
+myWatchedSeries.unshift("stranger things")
+
+console.log(myWatchedSeries);
+
 
 let c = 18
 
 let f = c/5*9+32
 
 console.log(`${c}°C is ${f}°F`);
+
---
+++ week3/day 3/scrip2.js
@@ -0,0 +1,55 @@
+
+// let mydata = {
+//     name : "israel",
+//     lastname : "adgo",
+//     age : 30
+// }
+
+// mydata.addsomething = "something"
+
+// mydata.age = 31
+
+// delete mydata.name
+
+// console.log(mydata);
+
+
+// let userdata = {
+//     name : "israel",
+//     password : "adgo",
+// }
+
+// let ary = [ "israel", "adgo"];
+
+// console.log(userdata);
+
+// console.log(ary);
+
+
+
+
+// let my_age = 31
+
+// if (my_age > 21) {
+//     console.log("can drink");
+// } else {
+//     console.log("cant drink");
+// }
+
+
+
+let password = "israel1111";
+
+if (password.length > 8) {
+    console.log('password is ok');
+
+} else if (password.length > 8 && password.length < 18) {
+    console.log('password' );
+}
+
+else {
+    console.log('password is to short')
+}
+
+
+
