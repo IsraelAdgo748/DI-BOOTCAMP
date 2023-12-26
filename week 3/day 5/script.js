1dcda65a1e5c3d9308601a05ba440171e6c61cf0
israel <israel748@gmail.com>
2023-12-26 21:04:58 +0200

    week 4 day1

---
+++ week 3/day 5/script.js
@@ -0,0 +1,29 @@
+let name = 5
+
+// name=4
+
+// const name2 = 8
+
+// if (condition) {
+//     let name3 = 5
+//     if (true) {
+        
+//     console.log(name3);
+//     }
+
+//     if (true){
+        
+//     console.log(name3);
+//     }
+    
+
+// }
+
+for (let i = 0; i < 5; i++) {
+    console.log("i = " + i)
+}
+
+let arr = [1, 4, 7, 10]
+    for (let i=0; i<arr.length; i++) {
+        console.log(arr[i]);
+    }
---
+++ week 4/day 1/script.js
@@ -0,0 +1,31 @@
+function sayHello (name , age, eyeColor='brown') {
+    console.log(`hello ${name}, you are ${age} years old,
+    your eyes are ${eyeColor}`);
+}
+
+sayHello('adi', 24, 'brown')
+sayHello('yan')
+
+
+
+function change (usd) {
+    return usd*3.63;
+}
+console.log(change(50));
+
+
+function change2(nis) {
+    console.log(nis*0.28);
+}
+change2(13)
+
+// function Age(myAge) {
+//     console.log();
+// }
+
+// Age(50)
+
+function test () {
+    
+    console.log(5);
+}
